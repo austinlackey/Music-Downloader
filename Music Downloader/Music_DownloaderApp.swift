03533c006d66106407ad89c25469939edcd820bf
Austin Lackey <austinblackey@gmail.com>
2026-04-08 20:24:32 -0600

    Initial Commit

---
+++ Music Downloader/Music_DownloaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  Music_DownloaderApp.swift
+//  Music Downloader
+//
+//  Created by Austin Lackey on 4/8/26.
+//
+
+import SwiftUI
+
+@main
+struct Music_DownloaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
